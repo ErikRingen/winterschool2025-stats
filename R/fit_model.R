614741b0622cee0ece9674bb42e31da89fcd2d7f
ErikRingen <ringen.erik@gmail.com>
2025-02-04 10:43:25 +0100

    fix re-writing of the fit_model function

--- R/fit_model.R
+++ R/fit_model.R
@@ -1,7 +1,11 @@
-fit_model <- function(data, age_moderator = TRUE){
-    if(age_moderator){
-        model <- lm(cognitive_flexibility ~ multilingual * age, data = data)
-    } else {
+fit_model <-
+function (data, age_moderator = TRUE) 
+{
+    if (age_moderator) {
+        model <- lm(cognitive_flexibility ~ multilingual * poly(age, 
+            2), data = data)
+    }
+    else {
         model <- lm(cognitive_flexibility ~ multilingual, data = data)
     }
     return(model)
--- _targets.R
+++ _targets.R
@@ -8,6 +8,6 @@ list(
     tar_target(data_file, "data/cognitive_flexibility.csv", format = "file"),
     tar_target(data, read_csv(data_file)),
     tar_target(model, fit_model(data)),
-    tar_target(treatment_effect, avg_comparisons(model, newdata = data, variables = "multilingual")),
+    tar_target(marginal_effect, avg_comparisons(model, newdata = data, variables = "multilingual")),
     tar_target(model_plot, plot_predictions(model, condition = list("multilingual")))
 )
